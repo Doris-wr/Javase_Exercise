113090dcc2e8d7908f16e46fcd457d3713235054
Doris-wr <52379704+Doris-wr@users.noreply.github.com>
2019-11-27 23:05:04 +0800

    Create contains.java
    查找是否包含关键字key是否在单链表中

---
+++ contains.java
@@ -0,0 +1,11 @@
+
+    public boolean contains(int key) {
+        Node cur = head;
+        while (cur != null) {
+            if (cur.val == key) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
