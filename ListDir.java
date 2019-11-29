32db4b57505a585fc43960e3e992741f867b6a98
Doris-wr <52379704+Doris-wr@users.noreply.github.com>
2019-11-29 21:36:56 +0800

    Create ListDir.java
    查看某个目录下都有什么文件

---
+++ ListDir.java
@@ -0,0 +1,16 @@
+import java.io.File;
+public class ListDir {
+    public static void listDir(File dir){
+        File[] files=dir.listFiles();
+        for(File file:files){
+            if(file.isDirectory()){
+                listDir(file);
+            }else{
+                System.out.println(file.getAbsolutePath());
+            }
+        }
+    }
+    public static void main(String[] args) {
+        listDir(new File("D:\\比特学习必备"));
+    }
+}
